059b77c0801eff1edea515de40442c396389dea8
VACHARA VIRIYAKUL <63010871@KMITL.AC.TH>
2020-08-11 11:46:06 +0700

    TestCommit

---
+++ Project2/Source.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(int argc, char const* argv[])
+{
+    unsigned long t;
+    int h, m, s;
+    printf("Enter your time (second) : ")
+    scanf("%lu", &t);
+    h = t / 3600;
+    t -= h * 3600;
+    m = t / 60;
+    t -= m * 60;
+    s = t;
+    if (t >= 0)
+    {
+        printf("%d hour(s) %d minute(s) %d second(s).", h, m, s);
+    }
+    else
+    {
+        printf("Error");
+    }
+
+
+    return 0;
+}
